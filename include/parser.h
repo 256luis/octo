4b94f10eedaca854e8818ffec98d27e670d8da77
luisbanez <luismbanez@gmail.com>
2025-04-04 22:35:53 +0800

    delete commented out dead code

--- include/parser.h
+++ include/parser.h
@@ -119,65 +119,10 @@ typedef struct Expression
     };
 } Expression;
 
-/* typedef enum StatementKind */
-/* { */
-/*     STATEMENTKIND_VARIABLEDECLARATION, */
-/*     STATEMENTKIND_FUNCTIONDECLARATION, */
-/*     STATEMENTKIND_FUNCTIONCALL, */
-/*     STATEMENTKIND_COMPOUND, */
-/*     STATEMENTKIND_RETURN, */
-/* } StatementKind; */
-
-/* typedef struct Statement */
-/* { */
-/*     StatementKind kind; */
-/*     union */
-/*     { */
-/*         struct */
-/*         { */
-/*             char* identifier; */
-/*             char* type; */
-/*             Expression* value; */
-/*         } variable_declaration; */
-
-/*         struct */
-/*         { */
-/*             StatementList statements; */
-/*             int statement_count; */
-/*         } compound; */
-
-/*         struct */
-/*         { */
-/*             char* identifier; */
-/*             char* return_type; */
-
-/*             // arrays to hold params info */
-/*             char** param_identifiers; */
-/*             char** param_types; */
-/*             int param_count; */
-
-/*             struct Statement* body; */
-/*         } function_declaration; */
-
-/*         struct */
-/*         { */
-/*             char* identifier; */
-/*             struct Expression** args; // array of expression pointers */
-/*             size_t arg_count; */
-/*         } function_call; */
-
-/*         struct */
-/*         { */
-/*             Expression* value; */
-/*         } return_statement; */
-/*     }; */
-/* } Statement; */
-
 typedef struct Parser
 {
     SourceCode source_code;
     TokenList tokens;
-    // TokenNode* current_token_node;
     int current_token_index;
     Token current_token;
     Token next_token;
@@ -188,7 +133,6 @@ void parser_free( Parser* parser );
 Expression* parser_parse( Parser* parser );
 
 void expression_print( Expression* expression );
-// void statement_print( Expression* statement );
 
 ExpressionList expression_list_new();
 void expression_list_free( ExpressionList expressions );
